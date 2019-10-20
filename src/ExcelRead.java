b9f73c2ba1dae7c57072b15cd46544c6dd0dd376
Waleed Javed <waleedjaved@hotmail.com>
2019-10-20 04:33:50 -0500

    6TH COMMIT

---
+++ src/ExcelRead.java
@@ -0,0 +1,23 @@
+import javafx.scene.control.Cell;
+/*
+public class ExcelRead {
+    public static void main(String[]args){
+        File file = new File( )
+        System.out.println("1");
+        FileInputStream fs=new FileInputStream(file);
+        System.out.println("3");
+        sheet sh=wb.getsheet("Sheet1");
+        int rowCount=sh.getLastRownum();
+        row rw;
+        Cell cell;
+
+        for(int i=0; i<rowCount ; i++){
+            rw=sh.getRow(i);
+            cell =rw.getCell(0);
+            System.out.println(cell.getStringcellvalue());
+        }
+    }
+}
+
+
+ */
