9964ab3410eff810d11ee88c744e0db0fef3ad96
Waleed Javed <waleedjaved@hotmail.com>
2019-10-20 04:33:01 -0500

    5TH COMMIT

---
+++ src/DataTest.java
@@ -0,0 +1,23 @@
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.openqa.selenium.WebDriver;
+/*
+import java.io.File;
+import java.io.FileInputStream;
+
+public class DataTest {
+    public static void main(String[] args) throws Exception {
+        File file =new File("C:\\Users\\Waleed\\Downloads\\try2.xls");
+        FileInputStream fs= new FileInputStream(file);
+        Workbook wb=new HSSFWorkbook(fs);
+        sheet sh =wb.getSheet("Dataset");
+        Browser browser =new Browser();
+        WebDriver driver = browser.invokeChromeBrowser();
+        int totalRows=sh.getLastRowNum();
+
+
+    }
+}
+
+
+ */
